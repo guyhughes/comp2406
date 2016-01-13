863d1e92fa4d257d32d1c5e8133596e6c91ca608
Guy Hughes <g@gxg.me>
2016-01-13 16:52:19 -0500

    think we done async now -- as shown to TA

--- simplegrep_async.js
+++ simplegrep_async.js
@@ -4,26 +4,31 @@
 // This version uses asynchronous I/O.
 //
 
-var fs = require('fs');
+var fs = require('fs')
 
 if (process.argv.length < 4) {
-    console.error('Not enough parameters given. Try this: ' +
-                  '"node simplegrep_async term filename.txt"'); 
-    process.exit(1);
+  console.error('Not enough parameters given. Try this: ' +
+                '"node simplegrep_async term filename.txt"') 
+  process.exit(1)
 }
+var re       = new RegExp(process.argv[2])
+var filename = process.argv[3]
+var outfile  = process.argv[4]
 
-var searchterm = process.argv[2];
-var filename = process.argv[3];
+var output = []
 
 var returnMatches = function(err, rawContents) {
 
-    var lines = rawContents.split('\n');
+  var lines = rawContents.split('\n')
 
-    lines.forEach(function(theLine) {
-        if (theLine.indexOf(searchterm) > -1) {
-            console.log(theLine);
-        }
-    });
+  lines.forEach(function(theLine) {
+    if (re.test(theLine)) {
+      output.push(theLine)
+    }
+  })
+  output.sort()
+  fs.writeFile(outfile, output.join('\n'))
+  console.log('All done!')
 }
 
-fs.readFile(filename, 'utf-8', returnMatches);
+fs.readFile(filename, 'utf-8', returnMatches)
