6446b3af7070b7e9855ac086c10b73c2b90ee1e8
Guy Hughes <g@gxg.me>
2016-01-13 16:32:01 -0500

    make grep_sync use regex

--- simplegrep_sync.js
+++ simplegrep_sync.js
@@ -4,22 +4,23 @@
 // This version uses synchronous I/O.
 //
 
-var fs = require('fs');
+var fs = require('fs')
 
 if (process.argv.length < 4) {
-    console.error('Not enough parameters given. Try this: ' +
-                  '"node simplegrep_sync term filename.txt"'); 
-    process.exit(1);
+  console.error('Not enough parameters given. Try this: ' +
+                '"node simplegrep_sync term filename.txt"')
+  process.exit(1)
 }
 
-var searchterm = process.argv[2];
-var filename = process.argv[3];
+var re = new RegExp(process.argv[2])
+var filename = process.argv[3]
 
-var rawContents = fs.readFileSync(filename, 'utf-8');
-var lines = rawContents.split('\n');
+var rawContents = fs.readFileSync(filename, 'utf-8')
+var lines = rawContents.split('\n')
 
+var i
 for (i = 0; i < lines.length; i++) {
-    if (lines[i].indexOf(searchterm) > -1) {
-        console.log(lines[i]);
-    }
+  if (re.test(lines[i])) {
+    console.log(lines[i])
+  }
 }
