e225eec57c4bd2d3f711c623a9e39d5ccad1b9f3
Guy Hughes <g@gxg.me>
2016-01-28 08:38:00 -0500

    done

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var debug = require('debug')('routes');
+
+var state = [];
+
+router.get('/', function(req, res, next) {
+  debug('Rendering the main page');
+  res.render('index', { title: 'COMP 2406 Simple form demo' });
+});
+
+router.post('/add', function(req, res) {
+    var obj = { name: req.body.name,
+		city: req.body.city,
+                country: req.body.country,
+                birthday: req.body.birthday,
+                email: req.body.email };
+    state.push(obj);
+    res.render('add', { title: 'Person just added', item: obj });
+});
+
+router.get('/list', function(req, res) {
+    res.render('list', { title: 'People Listing',  items: state});
+});
+
+module.exports = router;
